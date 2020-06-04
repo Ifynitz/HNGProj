2a81ad75cf68a10a94e362a2ab2dde40cec236ba
Ifynitz <66220317+Ifynitz@users.noreply.github.com>
2020-06-04 20:44:05 +0100

    Create Index.php
    Learning to commit file directly on git

---
+++ Index.php
@@ -0,0 +1,8 @@
+<?php
+$email = "ihemeify00@gmail.com";
+$name = "Iheme Ifeanyi";
+$id = "HNG-00599";
+$language = "PHP";
+echo "Hello World, this is $name with HNG ID $id using $language for stage 2 task";
+
+?>
